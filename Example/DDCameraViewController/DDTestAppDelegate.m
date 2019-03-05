1577ad885632f1564a79ebe37b57299501f9b5e8
Kirill Ushkov <kirill.u@itomy.ch>
2019-03-05 19:28:06 +0200

    DDOS-47 added test app delegate to speed up tests

---
+++ Example/DDCameraViewController/DDTestAppDelegate.m
@@ -0,0 +1,12 @@
+//
+//  DDTestAppDelegate.m
+//  DDCameraViewController_Tests
+//
+//  Copyright © 2019 kirill.u@itomy.ch. All rights reserved.
+//
+
+#import "DDTestAppDelegate.h"
+
+@implementation DDTestAppDelegate
+
+@end
