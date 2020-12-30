c0d36f97ea25ff812708d489f93791f1b637ab9a
George Ulanov <george.ulanov@itomy.ch>
2020-12-30 10:50:39 +0200

    DDOS-189 - added a description to DDScannerViewController.h

--- DDCameraViewController/Classes/DDScannerViewController.h
+++ DDCameraViewController/Classes/DDScannerViewController.h
@@ -21,12 +21,23 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @property (weak, nonatomic, nullable) id<DDScannerViewControllerDelegate> delegate;
 
+/**
+ Adds the ability to set possible scan types.
+ 
+ @param types The value of this property is an NSArray of NSStrings corresponding to AVMetadataObjectType strings defined in AVMetadataObject.h -- one for each metadata object type supported by the receiver. Clients may specify the types of objects they would like to process by calling setMetadataObjectTypes:.
+ */
 - (void)setMetadataObjectTypes:(NSArray<AVMetadataObjectType> * _Nullable)types;
 
 @end
 
 @protocol DDScannerViewControllerDelegate <NSObject>
 
+/**
+ Sent to the delegate object after scanning.
+
+ @param controller View controller object that initiated the request.
+ @param code The scanned information is sent as a string to the delegate object.
+ */
 - (void)scannerViewController:(DDScannerViewController *)controller didScanMachineReadableCode:(NSString * _Nullable)code;
 
 @end
