733a0cf7996ef1310af370c299d621106a6c36a4
George Ulanov <george.ulanov@itomy.ch>
2021-01-05 17:21:53 +0200

    DDOS-189 - changed s.source
    - changed DDScannerViewController.m

--- DDCameraViewController/Classes/DDScannerViewController.m
+++ DDCameraViewController/Classes/DDScannerViewController.m
@@ -9,8 +9,8 @@
 #import "DDCameraViewController_Private.h"
 
 @interface DDScannerViewController ()<AVCaptureMetadataOutputObjectsDelegate>
+@property (strong, nonatomic, nullable) NSArray<AVMetadataObjectType> *scannerMetadataObjectTypes;
 @property (strong, nonatomic, nullable) NSString *machineReadableCode;
-@property (strong, nonatomic, nullable) NSArray<AVMetadataObjectType> *metadataObjectTypes;
 @property (strong, nonatomic, nullable) AVCaptureMetadataOutput *output;
 @end
 
@@ -19,27 +19,27 @@
 - (void)initializeCaptureSession {
     [super initializeCaptureSession];
     _output = [[AVCaptureMetadataOutput alloc] init];
-    [self.captureSession addOutput:_output];
-    [_output setMetadataObjectsDelegate:self queue:dd_capture_session_queue()];
+    [self.captureSession addOutput:self.output];
+    [self.output setMetadataObjectsDelegate:self queue:dd_capture_session_queue()];
     [self setMetadataObjectTypes:nil];
 }
 
 - (void)setMetadataObjectTypes:(NSArray<AVMetadataObjectType> * _Nullable)types {
-    _metadataObjectTypes = types;
-    NSSet *set = [NSSet setWithArray:_metadataObjectTypes];
-    NSMutableSet *available = [NSMutableSet setWithArray:[_output availableMetadataObjectTypes]];
+    self.scannerMetadataObjectTypes = types;
+    NSSet *set = [NSSet setWithArray:self.scannerMetadataObjectTypes];
+    NSMutableSet *available = [NSMutableSet setWithArray:[self.output availableMetadataObjectTypes]];
     [available intersectSet:set];
-    [_output setMetadataObjectTypes:available.allObjects];
+    [self.output setMetadataObjectTypes:available.allObjects];
 }
 
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection {
     for (AVMetadataObject *current in metadataObjects) {
         if ([current isKindOfClass:[AVMetadataMachineReadableCodeObject class]]
-            && [_metadataObjectTypes containsObject:current.type]) {
+            && [self.scannerMetadataObjectTypes containsObject:current.type]) {
             NSString *code = [(AVMetadataMachineReadableCodeObject *)current stringValue];
             if ([self.delegate respondsToSelector:@selector(scannerViewController:didScanMachineReadableCode:)]
-                && ![code isEqualToString:_machineReadableCode]) {
-                _machineReadableCode = code;
+                && ![code isEqualToString:self.machineReadableCode]) {
+                self.machineReadableCode = code;
                 dispatch_async(dispatch_get_main_queue(), ^(void){
                     [self.delegate scannerViewController:self didScanMachineReadableCode:code];
                 });
